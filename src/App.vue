8178faa411285a2a33cb26d92a848f3f7ae54b7a
Bennet <11414655+bennet33@user.noreply.gitee.com>
2022-09-06 16:13:08 +0800

    feat:posscss配置

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>黑马头条</div>
+  <div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <button class="box">登录</button>
+  </div>
 </template>
 
 <script>
@@ -8,4 +16,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.box {
+  width: 700px;
+  height: 88px;
+}
+</style>
